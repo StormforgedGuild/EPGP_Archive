697e12daa05857bb6c592b08f6862a8e7b7ced9c
jamiecool <51167476+jamiecool@users.noreply.github.com>
2020-08-22 18:55:23 +0000

    no commas

--- scripts/export_members.php
+++ scripts/export_members.php
@@ -36,7 +36,7 @@ if ($result->num_rows > 0) {
       $earned = $pointsArray[1][0];
       $spent = $pointsArray[1][1] + 2000;
       $pr = $earned/$spent;
-      $output = $row["member_name"]. ", " .number_format($pr,2). ", " . number_format($earned,2). ", " . number_format($spent,2)."\n";
+      $output = $row["member_name"]. ", " .number_format($pr,2,'.',''). ", " . number_format($earned,2,'.',''). ", " . number_format($spent,2,'.','')."\n";
       echo $output;
       fwrite($myfile, $output);
     }
